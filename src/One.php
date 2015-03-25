5dca2c4da5284df28974746e2c212bacbb68e87d
huanggan <466927746@qq.com>
2015-03-25 09:38:38 +0800

    4
    4

--- src/One.php
+++ src/One.php
@@ -1,9 +1,13 @@
 <?php
 namespace Hg\One;
+use Hg\Hgtest\Test;
 class One{
     public function __toString() {
         echo 'this is '.__CLASS__;
     }
+	public getOther(){
+		return Hg\Hgtest\Test();
+	}
     
 }
 ?>
