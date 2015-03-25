ed699c31be118e4508a90226e476c573cd1a693d
huanggan <466927746@qq.com>
2015-03-25 10:03:51 +0800

    4
    4

--- src/One.php
+++ src/One.php
@@ -1,13 +1,17 @@
 <?php
-namespace Hg\One;
-use Hg\Hgtest\Test;
+namespace Hg\Package;
+use Hg\oo\Test;
 class One{
+    protected $otherName;
+//    public function __construct(Test $a) {
+//        $this->otherName = $a;
+//    }
     public function __toString() {
         echo 'this is '.__CLASS__;
     }
-	public getOther(){
-		return Hg\Hgtest\Test();
-	}
+    public function getOthers(){
+        return Test;
+    }
+
     
 }
-?>
